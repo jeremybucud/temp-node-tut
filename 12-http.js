2ce0e0cc70ec4e74309276b46c5d1b867cfdc689
jeremybucud <jeremyusis@gmail.com>
2022-07-07 14:40:33 +0800

    first commit

---
+++ 12-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+
+//req - incoming request
+//res - sending back
+const server = http.createServer((req,res)=>{
+    if(req.url === '/')
+    {
+        res.end('Welcome to our homepage')
+    }
+    if(req.url === '/about')
+    {
+        res.end('Here is our short history')
+    }
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page that you're looking for.</p>
+        <a href="/">back home</a>
+    `)
+})
+
+server.listen(5000)
---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+// Globals - no window
+
+//__dirname - path to the current directory
+//__filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+
+//console.log(__dirname)
+
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
+
